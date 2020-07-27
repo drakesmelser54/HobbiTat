01810c2ed597a22aa64d6e4b427b494b034f6d59
Drake Smelser <drakesmelser54@gmail.com>
2020-07-27 08:21:27 -0500

    change theme, add folder for artist skethes for logo

--- webPageShells/shells.js
+++ webPageShells/shells.js
@@ -1,4 +1,4 @@
 /*Toggle between hiding and showing hamburger drop down when clicked*/
-function showDropdown() {
+document.getElementById("hamburger-btn").addEventListener("click", function() {
   document.getElementById("hamburger-dropdown").classList.toggle("show");
-}
+});
