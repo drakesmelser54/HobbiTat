91ab100bc8e60347c92ed0a450d12832acd087bd
Drake Smelser <drakesmelser54@gmail.com>
2020-07-29 22:42:20 -0500

    add forms in place of text on web page shells. Add an altogether new form to take user profile info for their profile page

--- webPageShells/shells.js
+++ webPageShells/shells.js
@@ -2,3 +2,20 @@
 document.getElementById("hamburger-btn").addEventListener("click", function() {
   document.getElementById("hamburger-dropdown").classList.toggle("show");
 });
+
+/*Array of Artist Sketches*/
+const artistSketches = [
+  {
+    url:
+      "https://raw.githubusercontent.com/drakesmelser54/HobbiTat/master/Artist%20Sketches/artist%20white.png",
+    title: "chef sketch"
+  }
+];
+/*Populate artist sketches for nav bar*/
+const navBarSketches = document.querySelector(".hobbySketches");
+artistSketches.forEach(pic => {
+  let img = document.createElement("img");
+  img.src = pic.url;
+  img.alt = pic.title;
+  navBarSketches.appendChild(img);
+});
