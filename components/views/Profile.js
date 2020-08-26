68aa40469ee811be30f28262e1ccf9e9647259cb
Drake Smelser <drakesmelser54@gmail.com>
2020-08-25 19:53:38 -0500

    fixed style of profile page for user data to fit appropriately

--- components/views/Profile.js
+++ components/views/Profile.js
@@ -1,18 +1,21 @@
 export default () => `
-  <section class="profilePictureContainer">
-  </section>
-  <section class="lowerProfileContainer">
-    <div class="leftProfileContainer">
-      <h2>Name</h2>
-      <h4 id="username">Lorem ipsum</h4>
-      <h2>Location</h2>
-      <p>Aliquam ut ante</p>
-    </div>
-    <div class="rightProfileContainer">
-      <h2 class="profileHeaders">Hobbies</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <h2 class="profileHeaders">Projects</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-  </section>
+<div class="profile-card-aligner">
+  <div class="profile-card">
+    <section class="upperProfileContainer">
+      <img id="user-photo" src="https://raw.githubusercontent.com/drakesmelser54/HobbiTat/master/Images/oceanPhoto.png" alt="profile picture">
+    </section>
+    <section class="lowerProfileContainer">
+      <div class="leftProfileContainer">
+        <h2 id="user-name">Name</h2>
+        <h2 id="user-location">Location</h2>
+      </div>
+      <div class="rightProfileContainer">
+        <h3>Hobbies</h3>
+        <p id="user-hobbies">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h3>Project Links</h3>
+        <p id="user-links">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </section>
+  </div>
+</div>
 `;
