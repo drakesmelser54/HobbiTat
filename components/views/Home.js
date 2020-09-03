7235b2c3977e5fe5748d5625fe7568174b16e252
Drake Smelser <drakesmelser54@gmail.com>
2020-09-03 15:24:57 -0500

    add back-to-top btn on home page

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,7 @@
 export default () => `
 <section class="home-page-jumbotron">
   <div class="welcome-container">
-    <h1>HobbiTat</h1>
+    <h1 id="top-of-page">HobbiTat</h1>
     <a id="login-button" href="/Login" data-navigo>Login</a>
     <a id="signup-button" href="/Signup" data-navigo>Signup</a>
   </div>
@@ -17,5 +17,6 @@ export default () => `
   </div>
   <img class="home-photos" id="final-home-photo" src="https://raw.githubusercontent.com/drakesmelser54/HobbiTat/master/Images/videographer.jpg" alt="videographer">
   <h2 id="final-home-header">See what skills are out there. Learn something new!</h2>
+  <a id="back-to-top" href="#top-of-page">Top of Page</a>
 </section>
 `;
